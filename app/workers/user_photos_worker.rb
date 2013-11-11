faca217b8f64cda7f2592d7a22af40925e7b9008
Daniel Perez <tuvistavie@gmail.com>
2013-11-12 02:44:45 +0900

    Add gender to user.

--- app/workers/user_photos_worker.rb
+++ app/workers/user_photos_worker.rb
@@ -4,7 +4,7 @@ class UserPhotosWorker
   	user = User.find(id)
   	facebook_user = FbGraph::User.me(user.facebook_token)
 
-  	facebook_user.photos.each do |fb_photo|
+  	facebook_user.photos(limit: 100).each do |fb_photo|
       next if Photo.where(photo_id: fb_photo.identifier).exists?
       photo = Photo.new(user_id: user.id, photo_id: fb_photo.identifier, photo_url: fb_photo.source)
       if photo.save
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111163242) do
+ActiveRecord::Schema.define(version: 20131111172229) do
 
   create_table "photo_data", force: true do |t|
     t.integer  "user_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20131111163242) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "facebook_token"
+    t.integer  "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
