294f6ce71eefb47e0555c7002ca8a8df835c776c
Daniel Perez <tuvistavie@gmail.com>
2013-11-12 03:37:55 +0900

    Add page with photos.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
 
   def index
   end
 
   def show
-    @facebook_user = FbGraph::User.me(current_user.facebook_token).fetch
-    UserPhotosWorker.perform_async(current_user.id)
+    @score = current_user.scores.where(friend_id: @user.id).first
+    @photo_datas = PhotoData.where(user_id: @score.friend_id).includes(:photo)
   end
 
   def fetch_photo
@@ -17,4 +18,11 @@ class UsersController < ApplicationController
     @scores = current_user.scores.includes(:friend).order(score: :desc)
     @scores = @scores.where(gender: params[:gender]) if params[:gender]
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
+
