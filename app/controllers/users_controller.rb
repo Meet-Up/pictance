375f75c5960bc9ecaf281b7dc3c1b843e3a13521
akihokurino <a_k_i_h_o@i.softbank.jp>
2013-11-12 13:02:54 +0900

    現在の最新をpush

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ class UsersController < ApplicationController
   	@facebook_user = FbGraph::User.me(current_user.facebook_token).fetch
     UserPhotosWorker.perform_async(current_user.id)
     @scores = current_user.scores.includes(:friend).order(score: :desc)
-    @scores = @scores.where(gender: params[:gender]) if params[:gender]
+    @scores = @scores.joins(:user).where(users: { gender: params[:gender] }) if params[:gender]
+
+    #binding.pry
   end
 
   private
