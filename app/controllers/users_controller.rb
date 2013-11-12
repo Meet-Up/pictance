86e17b724bcad01d069f35cf5670b54b4dcbdee7
akihokurino <a_k_i_h_o@i.softbank.jp>
2013-11-12 11:40:35 +0900

    showページ、rankingページのフロント作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
   end
 
   def ranking
+  	@user = current_user
+  	@facebook_user = FbGraph::User.me(current_user.facebook_token).fetch
     UserPhotosWorker.perform_async(current_user.id)
     @scores = current_user.scores.includes(:friend).order(score: :desc)
     @scores = @scores.where(gender: params[:gender]) if params[:gender]
